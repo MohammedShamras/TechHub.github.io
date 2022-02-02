e38b7cd635654d9aba9c96f87cfdd3b92badfab0
MohammedShamras <93470890+MohammedShamras@users.noreply.github.com>
2022-02-02 20:35:20 +0530

    Add files via upload

--- src/firebase-config/firebase-config.js
+++ src/firebase-config/firebase-config.js
@@ -3,14 +3,14 @@ import { initializeApp } from 'firebase/app';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAWta1oke2LSI28B7jmFd8Q1AqlDozK59k",
-    authDomain: "note-app-57eed.firebaseapp.com",
-    projectId: "note-app-57eed",
-    storageBucket: "note-app-57eed.appspot.com",
-    messagingSenderId: "678023697832",
-    appId: "1:678023697832:web:3d10e7faa24da84a67b99d",
-    measurementId: "G-CMSWRPHSG7"
-  };
+  apiKey: "AIzaSyAWta1oke2LSI28B7jmFd8Q1AqlDozK59k",
+  authDomain: "note-app-57eed.firebaseapp.com",
+  projectId: "note-app-57eed",
+  storageBucket: "note-app-57eed.appspot.com",
+  messagingSenderId: "678023697832",
+  appId: "1:678023697832:web:e9d57a71b06b28ab67b99d",
+  measurementId: "G-QT9YSQ5DSV"
+};
 
 
 const app = initializeApp(firebaseConfig);
